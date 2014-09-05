2e13ba0fcbd1e7961220e4e2181c7243c7ceaebb
timbresmith <fitchmicah@gmail.com>
2014-09-05 18:13:34 +0200

    basic chat

---
+++ midi.js
@@ -0,0 +1,41 @@
+var midi = null;  // global MIDIAccess object
+
+function onMIDISuccess( midiAccess ) {
+  console.log( "MIDI ready!" );
+  midi = midiAccess;  // store in the global (in real usage, would probably keep in an object instance)
+}
+
+function onMIDIFailure(msg) {
+  console.log( "Failed to get MIDI access - " + msg );
+}
+
+navigator.requestMIDIAccess().then( onMIDISuccess, onMIDIFailure );
+
+function listInputsAndOutputs( midiAccess ) {
+  for (var input in midiAccess.inputs) {
+    console.log( "Input port [type:'" + input.type + "'] id:'" + input.id +
+      "' manufacturer:'" + input.manufacturer + "' name:'" + input.name +
+      "' version:'" + input.version + "'" );
+  }
+
+  for (var output in midiAccess.outputs) {
+    console.log( "Output port [type:'" + output.type + "'] id:'" + output.id +
+      "' manufacturer:'" + output.manufacturer + "' name:'" + output.name +
+      "' version:'" + output.version + "'" );
+  }
+}
+
+// receiving/sending midi
+
+function handleNetMIDI ( event ) {
+	// pass event to local stuff
+}
+
+function handleLocalMIDI( event ) {
+	// send midi to connected peers
+}
+
+function listenForMIDI( midiAccess, indexOfPort, handler ) {
+	midiAccess.inputs.entries[indexOfPort].onmidimessage = handler;
+}
+
