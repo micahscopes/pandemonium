fd2fd46319a7fd33cc61c88077263a7413f2bef8
timbresmith <fitchmicah@gmail.com>
2014-09-06 04:17:56 +0200

    this is good.

--- client.js
+++ client.js
@@ -1,28 +1,3 @@
-//
-//Copyright (c) 2014, Priologic Software Inc.
-//All rights reserved.
-//
-//Redistribution and use in source and binary forms, with or without
-//modification, are permitted provided that the following conditions are met:
-//
-//    * Redistributions of source code must retain the above copyright notice,
-//      this list of conditions and the following disclaimer.
-//    * Redistributions in binary form must reproduce the above copyright
-//      notice, this list of conditions and the following disclaimer in the
-//      documentation and/or other materials provided with the distribution.
-//
-//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-//ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-//LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-//CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-//SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-//INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-//CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-//POSSIBILITY OF SUCH DAMAGE.
-//
 var selfEasyrtcid = "";
 var connectList = {};
 var channelIsActive = {}; // tracks which channels are active
@@ -57,13 +32,13 @@ function connect() {
 	//setTimeout( function(){
 		var username = usernameField.value;
 		easyrtc.setUsername(username);
-		easyrtc.enableDebug(false);
 		easyrtc.enableDataChannels(true);
 		easyrtc.enableVideo(false);
+		easyrtc.enableVideoReceive(false);		
 		easyrtc.enableAudio(false);
-		easyrtc.enableVideoReceive(false);
 		easyrtc.enableAudioReceive(false);
 		easyrtc.setDataChannelOpenListener(openListener);
+		easyrtc.enableDebug(false);
 		easyrtc.setDataChannelCloseListener(closeListener);
 		easyrtc.setPeerListener(handleIncomingMessage);
 		easyrtc.setRoomOccupantListener(convertListToButtons);
@@ -78,13 +53,21 @@ function handleIncomingMessage(who, mstType, content) {
 			o.send(new Uint8Array([c[0],c[1],c[2]]));
 		}
 	});
+	addToConversation(content,who);
+}
+
+function addToConversation(content,who) {
 	content = JSON.stringify(content);
     content = content.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     content = content.replace(/\n/g, '<br />');
-    document.getElementById('conversation').innerHTML +=
-            "<b>" + easyrtc.idToName(who) + ":</b>&nbsp;" + content + "<br />";
-
+    document.getElementById('conversation').innerHTML =
+    "<b>" + easyrtc.idToName(who) + ":</b>&nbsp;" + content + "<br />" + 
+    document.getElementById('conversation').innerHTML;
+    var length = 5000;
+	document.getElementById('conversation').innerHTML = 
+		document.getElementById('conversation').innerHTML.substring(0,length)+"...";
 }
+
 
 function openListener(otherParty) {
     channelIsActive[otherParty] = true;
@@ -108,7 +91,7 @@ function ioConfiguration() {
 		checkbox.type = "checkbox";
 		inputsConfigArea.appendChild(label);
 		label.appendChild(checkbox);
-		label.innerHTML += input.name;
+		label.appendChild(document.createTextNode(input.name));
 		checkbox = document.getElementById(id);
 		checkbox.onchange = function(e) {
 			if(e.target.checked) {
@@ -130,8 +113,7 @@ function ioConfiguration() {
 		checkbox.type = "checkbox";
 		label.appendChild(checkbox);
 		outputsConfigArea.appendChild(label);
-		label.innerHTML += output.name;
-		checkbox = document.getElementById(id);
+		label.appendChild(document.createTextNode(output.name));
 		checkbox.onchange = function(e) {
 			 output.enabled = e.target.checked;
 		}
@@ -149,7 +131,8 @@ function convertListToButtons(roomName, occupantList, isPrimary) {
     var label, button;
     for (var easyrtcid in connectList) {
         var rowGroup = document.createElement("div");
-        var rowLabel = document.createTextNode(easyrtc.idToName(easyrtcid));
+        var rowLabel = document.createElement("b");
+        rowLabel.innerHTML = easyrtc.idToName(easyrtcid);
         rowGroup.appendChild(rowLabel);
 
         button = document.createElement('button');
@@ -184,7 +167,22 @@ function convertListToButtons(roomName, occupantList, isPrimary) {
 function updateButtonState(otherEasyrtcid) {
     var isConnected = channelIsActive[otherEasyrtcid];
     if(document.getElementById('connect_' + otherEasyrtcid)) {
-        document.getElementById('connect_' + otherEasyrtcid).disabled = isConnected;
+        document.getElementById('connect_' + otherEasyrtcid).innerHTML = (isConnected ? "Disconnect" : "Connect");
+        if(!isConnected) {
+			document.getElementById('connect_' + otherEasyrtcid).onclick =
+			function(easyrtcid) {
+				return function() {
+					startCall(easyrtcid);
+				};
+			}(otherEasyrtcid);
+		} else {
+			document.getElementById('connect_' + otherEasyrtcid).onclick =
+			function(easyrtcid) {
+				return function() {
+					easyrtc.hangup(easyrtcid);
+				};
+			}(otherEasyrtcid);
+		}
     }
     if( document.getElementById('send_' + otherEasyrtcid)) {
         document.getElementById('send_' + otherEasyrtcid).disabled = !isConnected;
@@ -243,13 +241,14 @@ function sendStuffP2P(otherEasyrtcid) {
         easyrtc.showError("NOT-CONNECTED", "not connected to " + easyrtc.idToName(otherEasyrtcid) + " yet.");
     }
 
-    addToConversation("Me", "msgtype", text);
+    addToConversation(text,"me");
     document.getElementById('sendMessageText').value = "";
 }
 
 
 function loginSuccess(easyrtcid) {
-    document.getElementById("iam").innerHTML = "I am " + easyrtc.idToName(easyrtcid) +" ("+easyrtcid+").";
+    document.getElementById("usernameConfig").innerHTML = "<h3>"+easyrtc.idToName(easyrtcid)+"</h3>";
+    document.getElementById("iam").innerHTML = "";
 }
 
 
